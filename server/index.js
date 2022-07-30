587bbb7adee6aafb7ff61b0d5f52daa9b723d687
Ali Husen <a.husen21@gmail.com>
2022-07-30 19:07:41 +0530

    connected mongoDB to the project with 2 models

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const port = process.env.PORT || 5000;
+const colors = require("colors");
+
+const schema = require("./schema/schema");
+const { graphqlHTTP } = require("express-graphql");
+const connectDB = require("./config/db");
 
 const app = express();
 
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV == "development" ? true : false,
+  }),
+);
 app.listen(port, console.log(`Server started on port ${port}`));
